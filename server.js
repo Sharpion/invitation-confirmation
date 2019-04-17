80eb48c68fd7485fb3857663f6a8311527206051
Felipe - Notebook <sharpion.k@gmail.com>
2019-04-17 13:39:24 -0300

    Changing from MySQL to MongoDB

--- server.js
+++ server.js
@@ -2,46 +2,85 @@
 // where your node app starts
 
 // init project
-const express   = require('express');
-const app       = express();
-const mysql     = require('mysql');
-const path      = require('path');
+var express     = require('express');        // call express
+var app         = express();                 // define our app using express
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
 
-//Environment variables
-const DB_PASS = process.env.DB_PASS;
-const DB_USER = process.env.DB_USER;
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors()); //Enable cross-origin requests
 
-const db = mysql.createConnection ({
-    host: 'mysql.casamentogife.com.br',
-    user: DB_USER,
-    password: DB_PASS,
-    database: 'gife_wedding'
+var port = process.env.PORT || 8080;        // set our port
+var dbURI = process.env.MONGODB_URI;
+
+var mongoose   = require('mongoose');
+mongoose.connect(dbURI); // connect to the database
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+    console.log('Mongoose default connection open to ' + dbURI);
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+    console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+    console.log('Mongoose default connection disconnected'); 
 });
 
-// connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
-});
-global.db = db;
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    console.log('Request received...');
+    next(); // make sure we go to the next routes and don't stop here
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'API app is up and running' });   
 });
+
+// on routes that end in /confirmation
+// ----------------------------------------------------
+router.route('/confirmation')
+
+  .get(function(req, res) {
+      Forecast.find(function(err, forecasts) {
+          if (err)
+              res.send(err);
+          
+          res.json(forecasts);
+      }).sort({name: -1})
+  });
+
+  router.route('/confirmation/:confirmation_id')
+  // get the forecast with that id (accessed at GET http://localhost:8080/api/forecasts/:forecast_id)
+  .get(function(req, res) {
+      Forecast.findById(req.params.forecast_id, function(err, forecast) {
+            if (err)
+                res.send(err);
+            res.json(forecast);
+      });
+});
+
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+var Confirmation     = require('./schema/confirmation');
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Open port: ' + port);
