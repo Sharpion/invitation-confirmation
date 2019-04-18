f9e8533a7f6e765923cb653ce90cd93f1fe584b3
Felipe - Notebook <sharpion.k@gmail.com>
2019-04-18 01:22:34 -0300

    Changing e-mail from TSL to SSL

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ dotenv.config();
 
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL,
         pass: process.env.E_PASS
